75da6db0dfe8b4f845225ecc814e4e1ac03cda78
shabakhsh14 <yosefyones1359@gmail.com>
2016-02-26 10:02:54 +0330

    Create seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -232,7 +232,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {117625029,184413821,},--Sudo users
+    sudo_users = {147368581,184413821,},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[]],
